f0d451686e980cb6c248d3a5c509692a34041b2f
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-13 11:49:10 +0200

    Added react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import { NextPageContext } from 'next';
+import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider, DehydratedState } from 'react-query';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type PageProps = {
+  dehydratedState?: DehydratedState;
+};
+
+type ExtendedAppProps<P = {}> = {
+  err?: NextPageContext['err'];
+} & AppProps<P>;
+
+function MyApp({ Component, pageProps }: ExtendedAppProps<PageProps>) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
