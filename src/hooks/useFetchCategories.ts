1647eb91d52374aed569681e3f095a4ca603f277
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-13 17:58:10 +0200

    Added prettier formating for typescript files (.ts)

--- src/hooks/useFetchCategories.ts
+++ src/hooks/useFetchCategories.ts
@@ -1,22 +1,22 @@
-import axios from "axios";
-import { useQuery } from "react-query";
-import { Category } from "types/Category";
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { Category } from 'types/Category';
 
 export const fetchCategories = async () => {
-	try{
-		const res = await axios.get(process.env.FETCH_CATEGORIES || '')
+  try {
+    const res = await axios.get(process.env.FETCH_CATEGORIES || '');
 
-		return res?.data?.categories
-	} catch(err) {
-		throw new Error('Something went wrong')
-	}
+    return res?.data?.categories;
+  } catch (err) {
+    throw new Error('Something went wrong');
+  }
 };
 
 export const useFetchCategories = () => {
-	const { data, isLoading, error } = useQuery('categories', fetchCategories, {
+  const { data, isLoading, error } = useQuery('categories', fetchCategories, {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
 
-	return {data, isLoading, error} as { data: Category[], isLoading: boolean, error: unknown}
+  return { data, isLoading, error } as { data: Category[]; isLoading: boolean; error: unknown };
 };
