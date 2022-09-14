75e99e33c5692546d35382d0ab4b896b22e4e326
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-14 13:43:46 +0200

    Fixed hamburger button animations

--- src/components/NavBar/NavBar.styles.tsx
+++ src/components/NavBar/NavBar.styles.tsx
@@ -12,7 +12,8 @@ export const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 20px;
+  height: 90px;
+  padding-inline: 10px;
 
   a {
     display: flex;
@@ -20,9 +21,9 @@ export const NavBar = styled.div`
 `;
 
 const buttonStyle = css`
-  height: 5px;
-  width: 40px;
-  background-color: ${({ theme }) => theme.colors.black};
+  height: 4px;
+  width: 36px;
+  background-color: #2e2e2e;
   transition-duration: 0.5s;
   border-radius: 100px;
 `;
@@ -47,29 +48,33 @@ export const HambuergerMenu = styled.button<HamburgerMenu>`
       content: '';
       position: absolute;
       left: 0;
-      top: -15.5px;
+      top: -12px;
       ${buttonStyle};
       ${({ $isCartOpen }) =>
-        $isCartOpen && `transform: rotateZ(-45deg) scaleX(0.75) translate(-20px, -6px);`};
+        $isCartOpen && `transform: rotateZ(-45deg) scaleX(0.75) translate(-12px, -4px);`};
     }
 
     &::after {
       content: '';
       position: absolute;
       left: 0;
-      bottom: -15.5px;
+      bottom: -12px;
       ${buttonStyle};
       ${({ $isCartOpen }) =>
-        $isCartOpen && `transform: rotateZ(45deg) scaleX(0.75) translate(-20px, 6px);`};
+        $isCartOpen && `transform: rotateZ(45deg) scaleX(0.75) translate(-12px, 4px);`};
     }
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    display: none;
   }
 `;
 
 export const SlideCart = styled.div`
   position: absolute;
-  width: 100%;
-  bottom: 0;
-  background-color: red;
+  top: 90px;
   left: 0;
-  display: none;
+  width: 100%;
+  height: calc(100vh - 90px);
+  background-color: grey;
 `;
