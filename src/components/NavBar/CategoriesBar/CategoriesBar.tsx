3d6f557f4d8658f7b054c77bf11c39807a07a4d5
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-21 16:54:08 +0200

    categories bar - final

--- src/components/NavBar/CategoriesBar/CategoriesBar.tsx
+++ src/components/NavBar/CategoriesBar/CategoriesBar.tsx
@@ -6,20 +6,16 @@ import SlideBar from 'components/SlideBar/SlideBar';
 const CategoriesBar = () => {
   const { data, error } = useFetchCategories();
 
-  return (
-    <>
-      {!error && (
-        <Styled.Wrapper>
-          <SlideBar>
-            {data?.map(({ idCategory, strCategory }) => (
-              <li key={idCategory}>
-                <Link href={`/categories/${strCategory}`}>{strCategory}</Link>
-              </li>
-            ))}
-          </SlideBar>
-        </Styled.Wrapper>
-      )}
-    </>
+  return error ? null : (
+    <Styled.Wrapper>
+      <SlideBar>
+        {data?.map(({ idCategory, strCategory }) => (
+          <li key={idCategory}>
+            <Link href={`/categories/${strCategory}`}>{strCategory}</Link>
+          </li>
+        ))}
+      </SlideBar>
+    </Styled.Wrapper>
   );
 };
 
