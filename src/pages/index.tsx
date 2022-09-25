b76a98720a0cddd2e4a02774c21c3d000f3e4436
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-25 19:27:14 +0200

    Added categories for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { dehydrate, QueryClient } from 'react-query';
-import { fetchCategories } from 'hooks/useFetchCategories';
+import { fetchCategories, useFetchCategories } from 'hooks/useFetchCategories';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Home() {
+  const { categories, error } = useFetchCategories();
+
   return (
     <div>
       <Head>
@@ -19,13 +23,37 @@ function Home() {
       </Head>
 
       <main style={{ height: '200vh' }}>
-        {/* <section>
-          {isLoading ? <p>Loading...</p> : null}
-          {error ? <p>Error</p> : null}
-          {data?.map(({ idCategory, strCategory, strCategoryThumb }) => (
-            <article key={idCategory}>{strCategory}</article>
-          ))}
-        </section> */}
+        <h1>Available categories:</h1>
+
+        {error ? (
+          <div>
+            <h2>Sorry we couldn't find any categories</h2>
+          </div>
+        ) : (
+          <section>
+            {categories?.map(({ idCategory, strCategory, strCategoryThumb }) => (
+              <Link
+                href={`/category/${strCategory}`}
+                key={idCategory}
+              >
+                <a>
+                  <article>
+                    <header>
+                      <h2>{strCategory}</h2>
+                    </header>
+
+                    <Image
+                      src={strCategoryThumb}
+                      alt={strCategory}
+                      width={300}
+                      height={300}
+                    />
+                  </article>
+                </a>
+              </Link>
+            ))}
+          </section>
+        )}
       </main>
     </div>
   );
