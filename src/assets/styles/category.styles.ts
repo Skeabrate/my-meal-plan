3a03e911894ebe1ac49d1ef4d76d88ea9f43b47f
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-09-26 23:29:27 +0200

    Category page animations

--- src/assets/styles/category.styles.ts
+++ src/assets/styles/category.styles.ts
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideIn = keyframes`
+	from{
+		transform: translateX(-10px);
+		opacity: 0;
+	} to {
+		transform: translateX(0);
+		opacity: 1;
+	}
+`;
 
 export const Intro = styled.section`
   display: grid;
@@ -17,6 +27,7 @@ export const Intro = styled.section`
     border-radius: 1rem;
     line-height: 1.5;
     box-shadow: 0 0 0 1px white, 0 0 0 2px ${({ theme }) => theme.colors.green};
+    animation: ${slideIn} 0.6s forwards;
   }
 
   ${({ theme }) => theme.mq.tablet} {
