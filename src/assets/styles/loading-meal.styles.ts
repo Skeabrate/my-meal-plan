ad2057b653767b6fdfeafea6c540606e5c2696e3
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-10-05 22:10:39 +0200

    Skeleton loading for meal page

---
+++ src/assets/styles/loading-meal.styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+
+// TODO: replace color to lightGrey, add animations
+export const Wrapper = styled.div`
+  p {
+    background-color: ${({ theme }) => theme.colors.lightGrey};
+  }
+`;
+
+export const Header = styled.div`
+  margin-bottom: 20px;
+
+  p {
+    &:first-child {
+      height: 40px;
+      width: 70%;
+      margin-bottom: 20px;
+    }
+
+    &:last-child {
+      background-color: ${({ theme }) => theme.colors.grey};
+      height: 60px;
+      width: 100%;
+    }
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 60px;
+
+    p {
+      &:first-child {
+        height: 60px;
+        margin-bottom: 0;
+      }
+
+      &:last-child {
+        width: 160px;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  ${({ theme }) => theme.mq.desktop} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 30px;
+  }
+`;
+
+export const Gallery = styled.div`
+  p {
+    background-color: ${({ theme }) => theme.colors.grey};
+
+    &:first-child {
+      height: 350px;
+      margin-bottom: 10px;
+    }
+
+    &:last-child {
+      height: 250px;
+    }
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    p {
+      &:first-child {
+        height: 600px;
+        margin-bottom: 30px;
+      }
+
+      &:last-child {
+        height: 400px;
+      }
+    }
+  }
+`;
+
+export const Tabs = styled.div`
+  margin-top: 30px;
+
+  div:first-child {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 8.5px;
+    margin-bottom: 20px;
+
+    p {
+      height: 60px;
+      background-color: ${({ theme }) => theme.colors.grey};
+    }
+  }
+
+  div:last-child {
+    display: grid;
+    grid-auto-rows: 60px;
+    row-gap: 8.5px;
+  }
+
+  ${({ theme }) => theme.mq.desktop} {
+    margin-top: 0;
+  }
+`;
--- src/pages/loading/meal.tsx
+++ src/pages/loading/meal.tsx
@@ -1,15 +1,55 @@
 import { useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import * as Styled from 'assets/styles/loading-meal.styles';
 
 const Loading = () => {
   const router = useRouter();
 
-  useEffect(() => {
-    router.replace(`/meal/${router.query.id}`);
-  });
+  // useEffect(() => {
+  //   router.replace(`/meal/${router.query.id}`);
+  // });
 
-  return <div>Loading...</div>;
+  return (
+    <Styled.Wrapper>
+      <Styled.Header>
+        <p></p>
+        <p></p>
+      </Styled.Header>
+
+      <Styled.Content>
+        <Styled.Gallery>
+          <p></p>
+          <p></p>
+        </Styled.Gallery>
+
+        <Styled.Tabs>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </Styled.Tabs>
+      </Styled.Content>
+    </Styled.Wrapper>
+  );
 };
 
 export default Loading as NextPage;
