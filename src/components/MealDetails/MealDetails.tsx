1d8ab35ee3b85e3d9c921ea139d8b2ea3b223bbf
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-10-10 16:08:18 +0200

    Added useMemo for meal details tab

--- src/components/MealDetails/MealDetails.tsx
+++ src/components/MealDetails/MealDetails.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import { useMemo, useState, ReactNode } from 'react';
 import * as Styled from './MealDetails.styles';
 
 type MealDetailsType = {
   tabs: {
     label: string;
-    Component: React.ReactNode;
+    Component: ReactNode;
   }[];
 };
 
 const MealDetails = ({ tabs = [] }: MealDetailsType) => {
   const [activeDetails, setActiveDetails] = useState(tabs[0]?.label);
-  const selectedTab = tabs.find((tab) => tab.label === activeDetails)?.Component;
+  const selectedTab = useMemo(
+    () => tabs.find((tab) => tab.label === activeDetails)?.Component,
+    [tabs, activeDetails]
+  );
 
   return (
     <Styled.MealDetails>
